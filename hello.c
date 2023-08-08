98beb998d5dbe22f26db0e8e82b929cb085df0ea
sofiaatuff <sofiahtuff@gmail.com>
2023-08-08 13:27:30 -0700

    new

---
+++ hello.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+printf("hello, world \n ");
+
+
+// this is incrementing
+int counter = 0;
+counter = counter + 1; 
+// can also do counter += 1;
+// can simplify it even more with counter++;
+
+
+// Conditional; it has a boolean expression that we were asking a question of
+if (x < y)
+{
+    printf("x is less than y\n");
+}
+else
+{
+    
+}
+
